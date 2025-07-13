f9ef217607fab00393cd24a9bd8c6f156024d412
PeterTerpe <peterxzq@gmail.com>
2025-07-14 00:41:42 +0800

    attempt to add item trigger

--- src/main/java/me/peterterpe/boatrace/RaceSession.java
+++ src/main/java/me/peterterpe/boatrace/RaceSession.java
@@ -2,18 +2,24 @@ package me.peterterpe.boatrace;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.title.Title;
+
 import java.util.*;
 
 public class RaceSession {
     private final RaceTrack track;
     private final Set<UUID> participants = new HashSet<>();
     private final Set<UUID> participated = new HashSet<>();
-    private final Map<Player, Long> startTimes = new HashMap<>();
+    private final Set<UUID> ready = new HashSet<>();
+    private long startTime;
     private boolean started = false;
     private Map<Player, RaceTimer> raceTimers = new HashMap<>();
     private boolean countdownActive = false;
@@ -28,21 +34,69 @@ public class RaceSession {
         }
         participants.add(player.getUniqueId());
         participated.add(player.getUniqueId());
-        broadcastToParticipants(Component.translatable("success.race.join", Component.text(player.getName()), Component.text(track.getName())));
+        giveReadyItem(player);
+        broadcast(Component.translatable("success.race.join", Component.text(player.getName()), Component.text(track.getName())));
     }
 
-    public void broadcastTitleToParticipants(Title title) {
+    public void broadcastTitle(Title title) {
         for (UUID uuid : participants) {
             Player p = Bukkit.getPlayer(uuid);
             BoatRace.getInstance().adventure().player(p).showTitle(title);
         }
     }
+    
+    public void broadcastActionBar(Component msg) {
+        for (UUID uuid : participants) {
+            Player p = Bukkit.getPlayer(uuid);
+            BoatRace.getInstance().adventure().player(p).sendActionBar(msg);
+        }
+    }
 
-    public void broadcastToParticipants(Component msg) {
+    public void broadcast(Component msg) {
         for (UUID uuid : participants) {
             Player p = Bukkit.getPlayer(uuid);
             BoatRace.getInstance().adventure().player(p).sendMessage(msg);
         }
+    }
+
+    public void giveReadyItem(Player player) {
+        ItemStack item = new ItemStack(Material.RED_DYE);
+        ItemMeta meta = item.getItemMeta();
+        meta.displayName(Component.translatable("item.notready"));
+        item.setItemMeta(meta);
+        player.getInventory().setItem(8, item);
+        player.updateInventory();
+    }
+
+    public void changeItem(Player player) {
+        Inventory inv = player.getInventory();
+        ItemStack oldItem = inv.getItem(8);
+        if (oldItem != null && oldItem.getType() == Material.GREEN_DYE && oldItem.hasItemMeta()) {
+            giveReadyItem(player);
+            ready.remove(player.getUniqueId());
+            return;
+        }
+        ready.add(player.getUniqueId());
+        ItemStack newItem = new ItemStack(Material.GREEN_DYE);
+        ItemMeta meta = newItem.getItemMeta();
+        meta.displayName(Component.translatable("item.ready"));
+        newItem.setItemMeta(meta);
+        player.getInventory().setItem(8, newItem);
+        player.updateInventory();
+        if (participants.size() == ready.size()) {
+            startCountdown(5);
+            for (UUID uuid : ready) {
+                Player p = Bukkit.getPlayer(uuid);
+                p.getInventory().setItem(8, null);
+                p.updateInventory();
+            }
+            ready.clear();
+        }
+    }
+
+    public void removeItem(Player player) {
+        player.getInventory().setItem(8, null);
+        player.updateInventory();
     }
 
     public void startCountdown(int seconds) {
@@ -53,18 +107,18 @@ public class RaceSession {
         }
         this.countdownActive = true;
         tpAllStartRegion();
-        broadcastToParticipants(Component.translatable("race.start.message", Component.text(track.getName())));
+        broadcast(Component.translatable("race.start.message", Component.text(track.getName())));
         this.started = true;
         new BukkitRunnable() {
             int timer = seconds;
             @Override
             public void run() {
                 if (timer <= 0) {
-                    broadcastTitleToParticipants(Title.title(Component.translatable("race.go"), Component.empty()));;
+                    broadcastTitle(Title.title(Component.translatable("race.go"), Component.empty()));;
                     for (UUID uuid : participants) {
                         Player player = Bukkit.getPlayer(uuid);
                         player.playSound(player.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_1, 1f, 1f);
-                        startTimes.put(player, System.currentTimeMillis());
+                        startTime = System.currentTimeMillis();
                     }
                     for (UUID uuid : participants) {
                         Player player = Bukkit.getPlayer(uuid);
@@ -75,7 +129,7 @@ public class RaceSession {
                     countdownActive = false;
                     this.cancel();
                 } else {
-                    broadcastTitleToParticipants(Title.title(Component.translatable("race.countdown", Component.text(timer)), Component.empty()));
+                    broadcastTitle(Title.title(Component.translatable("race.countdown", Component.text(timer)), Component.empty()));
                     for (UUID uuid : participants) {
                         Player player = Bukkit.getPlayer(uuid);
                         player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1f, 1f);
@@ -92,9 +146,9 @@ public class RaceSession {
      */
     public void checkFinish(Player player) {
         if (!started) return;
-        if (!startTimes.containsKey(player)) return;
+        if (!participants.contains(player.getUniqueId())) return;
         player.playSound(player.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1f, 1f);
-        long elapsed = System.currentTimeMillis() - startTimes.get(player);
+        long elapsed = System.currentTimeMillis() - startTime;
         for (UUID uuid : participated) {
             Bukkit.getPlayer(uuid).sendMessage(Component.translatable("race.player.finish", Component.text(player.getName()), Component.text(track.formatTime(elapsed))));
         }
@@ -116,8 +170,8 @@ public class RaceSession {
     /** Remove player from participants and their timer */
     public void stop(UUID uuid) {
         participants.remove(uuid);
+        ready.remove(uuid);
         Player player = Bukkit.getPlayer(uuid);
-        startTimes.remove(player);
         raceTimers.get(player).stop();
         raceTimers.remove(player);
         // Change the status to not started to allow future joins.
@@ -140,6 +194,10 @@ public class RaceSession {
         }
     }
 
+    public void markReady(UUID uuid) {
+        ready.add(uuid);
+    }
+
     public boolean isRunning() {
         return started;
     }
@@ -159,4 +217,6 @@ public class RaceSession {
     }
 
     public RaceTrack getTrack() { return track; }
+
+    public Set<UUID> getReady() { return ready; }
 }

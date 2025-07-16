PeterTerpe/BoatRace

commit ce511bc2e55700962c7cd0b1ef76bdd6fa2dfa8d

Bug fix fixed error which occurs when player disconnected before the session finishes

src/main/java/me/peterterpe/boatrace/RaceSession.java
@@ -125,7 +125,7 @@ public class RaceSession {
             @Override
             public void run() {
                 if (timer <= 0) {
-                    broadcastTitle(Title.title(Component.translatable("race.go"), Component.empty()));;
+                    broadcastTitle(Title.title(Component.translatable("race.go"), Component.empty()));
                     for (UUID uuid : participants) {
                         Player player = Bukkit.getPlayer(uuid);
                         player.playSound(player.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_1, 1f, 1f);
@@ -160,9 +160,12 @@ public class RaceSession {
         if (!started) return;
         if (!participants.contains(player.getUniqueId())) return;
         player.playSound(player.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1f, 1f);
+        stop(player.getUniqueId());
         long elapsed = System.currentTimeMillis() - startTime;
         for (UUID uuid : participated) {
-            Bukkit.getPlayer(uuid).sendMessage(Component.translatable("race.player.finish", Component.text(player.getName()), Component.text(track.formatTime(elapsed))));
+            Player p = Bukkit.getPlayer(uuid);
+            if (p == null) continue;
+            p.sendMessage(Component.translatable("race.player.finish", Component.text(player.getName()), Component.text(track.formatTime(elapsed))));
         }
         player.sendMessage(Component.translatable("race.finished"));
         if (track.addTime(player.getUniqueId(), elapsed)) {
@@ -170,7 +173,6 @@ public class RaceSession {
             RaceTrackManager.getInstance().updateLeaderboardHologram(track);
             player.sendMessage(Component.translatable("top5.congrats", Component.text("The Argument")));
         }
-        stop(player.getUniqueId());
         PersonalRecordsManager.getInstance().getRecord(player.getUniqueId()).recordTime(track.getName(), elapsed);
     }
     /** Remove all participants and their timers and set started to false */

pom.xml
@@ -6,7 +6,7 @@
 
     <groupId>me.peterterpe.boatrace</groupId>
     <artifactId>boatrace</artifactId>
-    <version>v1.3.0-beta</version>
+    <version>v1.3.0-beta.1</version>
 
     <properties>
         <maven.compiler.source>17</maven.compiler.source>

src/main/resources/plugin.yml
@@ -1,5 +1,5 @@
 name: BoatRace
-version: 1.3.0-beta
+version: 1.3.0-beta.1
 main: me.peterterpe.boatrace.BoatRace
 api-version: 1.21
 author: PeterTerpe

commit fc85decac2878cb71bd1e7e68133f36b801b10c2

fixed issue introduced in ce511bc2e55700962c7cd0b1ef76bdd6fa2dfa8d

src/main/java/me/peterterpe/boatrace/RaceSession.java
@@ -160,14 +160,13 @@ public class RaceSession {
         if (!started) return;
         if (!participants.contains(player.getUniqueId())) return;
         player.playSound(player.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1f, 1f);
-        stop(player.getUniqueId());
         long elapsed = System.currentTimeMillis() - startTime;
         for (UUID uuid : participated) {
             Player p = Bukkit.getPlayer(uuid);
             if (p == null) continue;
             p.sendMessage(Component.translatable("race.player.finish", Component.text(player.getName()), Component.text(track.formatTime(elapsed))));
         }
-        player.sendMessage(Component.translatable("race.finished"));
+        stop(player.getUniqueId());
         if (track.addTime(player.getUniqueId(), elapsed)) {
             StorageManager.getInstance().saveTrack(track);
             RaceTrackManager.getInstance().updateLeaderboardHologram(track);
@@ -193,6 +192,11 @@ public class RaceSession {
         // Change the status to not started to allow future joins.
         if (participants.isEmpty()) {
             this.started = false;
+            for (UUID playerID : participated) {
+                Player p = Bukkit.getPlayer(playerID);
+                if (p == null) continue;    
+                p.sendMessage(Component.translatable("race.finished"));
+            }
             participated.clear();
         }
     }